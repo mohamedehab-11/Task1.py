c40b1c21b448f9dfbda63ebe92f86fac7e12ec4e
mohamedehab-11 <mohamedehabnoby2292009@gmail.com>
2025-05-03 12:35:20 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,47 @@
+#print("____برنامج يحسب عدد الحروف في الكلمه ________ ")
+#a=input("write the word \n")
+#print(a)
+#print(int(len(a)))
+
+#print("________برنامج يحسب عدد الكلمات في الجمله_____")
+#t=input("write the sentens\n")
+#w=t.split()
+#a=len(w)
+#print(f"عدد الحرف ={int(len(t))}")
+#print(w)
+#print(f"عدد الكلمات ={a}")
+
+#print("___برنامج يعرض كل حرف اكرر كام مره ___")
+#t=input("write the word\t")
+#f={}
+#for c in t:
+#    if c in f:
+#        f[c]+=1
+#    else:
+#        f[c]=1   
+#print(t)
+#print(f)
+
+#print("___برنامج يعرض كل الكلمات اكرر كام مره ___")
+#t=input("write the sentens\t")
+#w=t.split()
+#f={}
+#for c in w:
+#    if c in f:
+#        f[c]+=1
+#    else:
+#        f[c]=1   
+#print(t)
+#print(f)        
+
+#print("____برنامج بيقولي الارقام الزوجيه  ____")
+#for i in range(0,103):
+#    if int(i)%2==0:
+#        print(i)
+
+
+
+
+
+
+
